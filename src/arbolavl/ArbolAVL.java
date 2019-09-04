655a45e49e40b4fd7d1b49db24a49a99436f883b
Zweinren <sguerreroa@uninorte.edu.co>
2019-09-04 18:46:58 -0500

    Push inicial

---
+++ src/arbolavl/ArbolAVL.java
@@ -0,0 +1,44 @@
+
+package arbolavl;
+
+
+public class ArbolAVL {
+
+    public static void main(String[] args) {
+        Nodo raiz=null;
+        
+        //Creación del árbol
+        int n = (int)(Math.random()*20), x;
+        System.out.println("Cantidad de números: "+n);        
+        for (int i = 0; i < n; i++) {
+            x=(int)(Math.random()*100);
+            System.out.print(x+" ");
+            raiz=Metodos.insertarAVL(raiz,x);
+        }
+        
+        //Recorridos
+        System.out.println("\nRecorrido Preorden:");
+        Recorridos.preOrdenR(raiz);
+        System.out.println("\nRecorrido Inorden:");
+        Recorridos.inOrdenR(raiz);
+        System.out.println("\nRecorrido Postorden:");
+        Recorridos.postOrdenR(raiz);
+        System.out.println("\nRecorrido Niveles:");
+        Recorridos.nivelesNR(raiz);
+        
+        //Propiedades
+        System.out.println("\nLa altura de la raiz es: "+Propiedades.altura(raiz));
+        System.out.println("El factor de balance de la raiz es: "+Propiedades.factorBalance(raiz));
+        System.out.println("El elemento máximo del árbol es: "+Propiedades.maximo(raiz).getDato());        
+        System.out.println("El nivel del elemento máximo es: "+Propiedades.obtenerNivel(raiz,Propiedades.maximo(raiz).getDato(),0));
+        
+        //Borrar
+        x=(int)(Math.random()*10);
+        System.out.println("Borrar el elemento: "+x);
+        raiz = Metodos.borrarAVL(raiz, x);
+        System.out.println("Recorrido Niveles:");
+        Recorridos.nivelesNR(raiz);
+        
+    }
+    
+}
---
+++ src/arbolavl/Recorridos.java
@@ -0,0 +1,68 @@
+package arbolavl;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Recorridos {    
+    public static void preOrdenR(Nodo raiz){
+        if (raiz!=null) {
+            System.out.print(raiz.getDato()+" ");
+            preOrdenR(raiz.getIzquierdo());
+            preOrdenR(raiz.getDerecho());
+        }
+    }
+    public static void preOrdenNR(Nodo raiz){
+        Stack<Nodo> pila = new Stack();
+        do{
+            if(raiz!=null){
+                System.out.println(raiz.getDato()+" ");                
+                pila.add(raiz);
+                raiz=raiz.getIzquierdo();
+            }else{
+                raiz=pila.pop();
+                raiz=raiz.getDerecho();
+            }
+        }while(!pila.isEmpty() || raiz!=null);
+    }
+    public static void inOrdenR(Nodo raiz){
+        if (raiz!=null) {
+            inOrdenR(raiz.getIzquierdo());
+            System.out.print(raiz.getDato()+" ");
+            inOrdenR(raiz.getDerecho());
+        }
+    }
+    public static void inOrdenNR(Nodo raiz){
+        Stack<Nodo> pila = new Stack();
+        do {
+            if (raiz != null) {
+                pila.add(raiz);
+                raiz = raiz.getIzquierdo();
+            } else {
+                raiz = pila.pop();
+                System.out.print(raiz.getDato() + " ");
+                raiz = raiz.getDerecho();
+            }
+        } while (!pila.isEmpty() || raiz != null);
+    }
+    public static void postOrdenR(Nodo raiz){
+        if (raiz!=null) {
+            postOrdenR(raiz.getIzquierdo());
+            postOrdenR(raiz.getDerecho());
+            System.out.print(raiz.getDato()+" ");
+        }
+    }
+    public static void nivelesNR(Nodo raiz){
+        Queue<Nodo> cola = new LinkedList<Nodo>();
+        cola.add(raiz);        
+         while(!cola.isEmpty()){
+             raiz = cola.remove();
+             System.out.print(raiz.getDato()+" ");
+             if (raiz.getIzquierdo()!=null) 
+                cola.add(raiz.getIzquierdo());
+             if(raiz.getDerecho()!=null)
+                 cola.add(raiz.getDerecho());             
+         }
+    }
+    
+}
